b3a8d0135e966dac59fbcdc5379e72014f7eaf1c
leandro <leandromarcosdedeus@gmail.com>
2025-05-04 20:59:33 -0300

    add component login 1.0

---
+++ frontend/src/app/components/auth/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  //usar modulo e não por component
+  //standalone: true,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', []],
+      senha: ['', []]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+      // Chamar API de autenticação aqui
+    }
+  }
+}

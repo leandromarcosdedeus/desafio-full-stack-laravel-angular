5c87ca82451e418f96378f35c9a2c760483f3153
leandro <leandromarcosdedeus@gmail.com>
2025-05-06 20:15:52 -0300

    Criando clinic create component

--- frontend/src/app/components/clinic/clinic-routing.module.ts
+++ frontend/src/app/components/clinic/clinic-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClinicListComponent } from './clinic-list/clinic-list.component';
+import { ClinicCreateComponent } from './clinic-create/clinic-create.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ClinicListComponent
+  },
+  {
+    path: 'create',
+    component: ClinicCreateComponent
   }
 ];
 

a3b11157aaf761e8a65765a85e73971199188b8a
leandro <leandromarcosdedeus@gmail.com>
2025-05-07 12:23:28 -0300

    model do crud clinics

---
+++ backend/app/Models/Regionals.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Regionals extends Model
+{
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function clinics()
+    {
+        return $this->hasMany(Clinic::class);
+    }
+}

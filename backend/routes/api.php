c5fbdeb5c1d4cef0de336e8af5e1743f923509d0
leandro <leandromarcosdedeus@gmail.com>
2025-05-07 12:23:54 -0300

    rotas com middleware de auth

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,16 +4,18 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::group(['middleware' => 'api'], function () {
-    Route::post('login', 'AuthController@login');
+
+Route::post('login', 'AuthController@login');
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('clinic', 'ClinicController@index');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
     Route::post('signup', 'AuthController@signup');
-});
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
 
 
